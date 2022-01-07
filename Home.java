badb81d08cad8d218d9917f1736c745ed75827db
Willthepenguin <87334831+Willthepenguin@users.noreply.github.com>
2022-01-07 08:43:25 -0500

    Update with Worlds

---
+++ Home.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Home here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Home extends World
+{
+
+    /**
+     * Constructor for objects of class Home.
+     * 
+     */
+    public Home()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
--- ScoreScreen.java
+++ ScoreScreen.java
@@ -1,21 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class ScoreScreen here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class ScoreScreen extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class ScoreScreen.
      * 
      */
-    public MyWorld()
+    public ScoreScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1); 
+        super(600, 400, 1); 
     }
 }
