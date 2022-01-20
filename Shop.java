f32267e9b2353fad95e0a7b56f1f6354c4a1f662
JackyFT <349970657@gapps.yrdsb.ca>
2022-01-19 23:54:10 -0500

    shop

--- Shop.java
+++ Shop.java
@@ -13,16 +13,20 @@ public class Shop extends World implements Clickable
      * Constructor for objects of class Shop.
      * 
      */
-    private Button healthPot;
-    private Button grenade;
-    private Button speedPot;
-    private Button damagePot;
-    private Button attackSpeedPot;
+    Button healthPot;
+    Button grenade;
+    Button speedPot;
+    Button damagePot;
+    Button attackSpeedPot;
+    Label shop;
     public Shop()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
+        shop = new Label("Shop", 50);
+        int labelWidth = (int) (getWidth() * 0.5);
+        int labelHeight = (int) (getHeight() *0.05);
+        addObject(shop, labelWidth, labelHeight);
         
     }
     public void onClick()
